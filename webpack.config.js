31139bd5e4ddc73fd35ad415ac31c5bf1dc8d63e
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-11-14 16:44:59 +0000

    Set up with webpack and npm

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// Webpack uses this to work with directories
+const path = require('path');
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: './index.js',
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'physics.js'
+  },
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript 
+  // minifying and other things, so let's set mode to development
+  mode: 'development'
+};
